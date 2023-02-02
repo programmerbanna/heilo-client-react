e07fe8509969ad4b37c8b600ab719af1c3e1f7bd
programmerbanna <programmerbanna@gmail.com>
2023-02-03 02:48:22 +0600

    layouts modified

--- src/shared/components/Layouts/Layouts.jsx
+++ src/shared/components/Layouts/Layouts.jsx
@@ -1,14 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { clx } from "shared/configs";
 
 // custom imports
 import Sidebar from "shared/widgets/Dashboard/Sidebar";
 
-const Layouts = () => {
+const Layouts = ({ className }) => {
   return (
-    <>
+    <div className="max-w-[calc(1673px+253px)] mx-auto bg-primaryDark min-h-screen flex">
       <Sidebar />
-      <Outlet />
-    </>
+      <div className={clx("py-4 pr-4 w-[1673px] min-h-ful", className)}>
+        <Outlet />
+        <div className="w-full min-h-full bg-white">
+          This white area is just for showcasing. for developing your ui just go
+          to file- src/shared/Layouts and simply comment this out
+        </div>
+      </div>
+    </div>
   );
 };
 
