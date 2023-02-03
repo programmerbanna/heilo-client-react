b627a2099d15eebe817f5cd5029e35ec01a7b9a1
programmerbanna <programmerbanna@gmail.com>
2023-02-04 00:27:01 +0600

    sub route and teacher route done

--- src/shared/route/sub-routes/subRoute.jsx
+++ src/shared/route/sub-routes/subRoute.jsx
@@ -1,18 +1,22 @@
+import { Admin } from "views/admin";
+import { Teacher } from "views/teacher";
 import adminSubRoute from "./adminSubRoute";
 import studentSubRoute from "./studentSubRoute";
 import teacherSubRoute from "./teacherSubRoute";
 
 const subRoute = [
   {
-    path: "/",
+    path: "/student",
     children: studentSubRoute,
   },
   {
-    path: "/",
+    path: "/teacher",
+    element: <Teacher />,
     children: teacherSubRoute,
   },
   {
-    path: "/",
+    path: "/admin",
+    element: <Admin />,
     children: adminSubRoute,
   },
 ];
