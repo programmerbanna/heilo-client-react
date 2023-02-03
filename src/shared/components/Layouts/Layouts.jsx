a163c39431471676afcdb01e0c2b9541695fee1f
programmerbanna <programmerbanna@gmail.com>
2023-02-03 21:27:14 +0600

    issue resolved

--- src/shared/components/Layouts/Layouts.jsx
+++ src/shared/components/Layouts/Layouts.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
-import { clx } from "shared/configs";
 
 // custom imports
+import { clx } from "shared/configs";
 import Sidebar from "shared/widgets/dashboard/sidebar";
 
 const Layouts = ({ className }) => {
