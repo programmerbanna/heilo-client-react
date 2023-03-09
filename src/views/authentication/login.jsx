505532804773f79885d8e646b1556d96f467d27d
programmerbanna <programmerbanna@gmail.com>
2023-03-09 14:13:12 +0600

    login api integrate

--- src/views/authentication/login.jsx
+++ src/views/authentication/login.jsx
@@ -1,93 +1,82 @@
-// import axios from "axios";
-// import { signIn } from "next-auth/react";
-// import Head from "next/head";
-
 import { useState } from "react";
 
-
-import { toast ,Toaster} from "react-hot-toast";
-import { Button } from 'shared/components/button';
-import { Link } from 'react-router-dom';
+import { toast, Toaster } from "react-hot-toast";
+import { Button } from "shared/components/button";
+import { Link } from "react-router-dom";
 import { InputBox } from "shared/components/input/input";
-
-// const Props = {};
+import { usePostData } from "shared/hooks/useFetch";
 
 const Login = (Props) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const { mutate } = usePostData("/user/login", "/user/login");
 
-    const handleLogin = async () => {
-        if (!email || !password) {
-            console.log("login");
-            toast.error("Please fill up all fields");
-            return;
-        }
-        toast.loading("Waiting for Response");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        const data = {
-            email: email,
-            password: password,
-        };
+  const handleLogin = async () => {
+    if (!email || !password) {
+      console.log("login");
+      toast.error("Please fill up all fields");
+      return;
+    }
+    toast.loading("Waiting for Response");
 
-        // const res = await signIn("credentials", { ...data, redirect: false });
-        // console.log(res?.status);
-        toast.dismiss();
-
-        //     if (res?.status !== 200) {
-        //       toast.error("Invalid email or password");
-        //       return;
-        //     } else {
-        //       toast.success("Login Successful");
-        //       router.push("/student/dashboard");
-        //     }
+    const data = {
+      email: email,
+      password: password,
     };
 
-    return (
+    mutate(data, {
+      onSuccess: () => {
+        toast.success("Login successfull");
+      },
+      onError: (error) => {
+        toast.error(`Something went wrong: ${error}`);
+      },
+    });
 
-        <div className="flex justify-center items-center h-screen relative overflow-hidden">
-            <div className="phase2-wave absolute top-0 flex flex-wrap justify-center items-center bg-wave2  bg-center bg-cover">
+    toast.dismiss();
+  };
 
+  return (
+    <div className="relative flex items-center justify-center h-screen overflow-hidden">
+      <div className="absolute top-0 flex flex-wrap items-center justify-center bg-center bg-cover phase2-wave bg-wave2">
+        <div className="z-20 -mt-8 min-w-xs form-modal">
+          <h1 className="mx-4 my-8 text-xl text-primaryDark">Welcome!</h1>
+          <div className="flex flex-col items-center mx-9 mt-14">
+            <InputBox
+              className="pb-5"
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Email"
+            />
+            <InputBox
+              className="pb-5 mt-5"
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password"
+            />
 
-                <div className="min-w-xs form-modal z-20 -mt-8">
-                    {/* <h1 className="text-primaryDark font-semibold text-2xl text-right mx-2">
-              x
-            </h1> */}
-                    <h1 className="text-primaryDark text-xl mx-4 my-8">Welcome!</h1>
-                    <div className="mx-9 mt-14 flex flex-col items-center">
-                        <InputBox
-                            className="pb-5"
-                            onChange={(e) => setEmail(e.target.value)}
-                            type="email"
-                            placeholder="Email"
-                        />
-                        <InputBox
-                            className="pb-5 mt-5"
-                            onChange={(e) => setPassword(e.target.value)}
-                            type="password"
-                            placeholder="Password"
-                        />
-
-                        <Button onClick={handleLogin}
-                            icon="uil uil-sign-in-alt" className="p-2   rounded-md bg-primaryDark text-white"
-                            label="Login" >
-
-                        </Button>
-                        <Link
-                            className="text-textSecondary cursor-pointer my-7"
-                            to="/registration"
-                        >
-                            Create Account
-                        </Link>
-                        <p className="text-textSecondary cursor-pointer">
-                            Forget Password ?
-                        </p>
-                        <Toaster position="bottom-right" reverseOrder={false} />
-                    </div>
-                </div>
-            </div>
+            <Button
+              onClick={handleLogin}
+              //   icon="uil uil-sign-in-alt"
+              className="p-2 text-white rounded-md bg-primaryDark"
+              label="Login"
+            ></Button>
+            <Link
+              className="cursor-pointer text-textSecondary my-7"
+              to="/registration"
+            >
+              Create Account
+            </Link>
+            <p className="cursor-pointer text-textSecondary">
+              Forget Password ?
+            </p>
+            <Toaster position="bottom-right" reverseOrder={false} />
+          </div>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
