90a86ecd60709c96be963bcd6597931e7f5ebc1e
programmerbanna <programmerbanna@gmail.com>
2023-03-15 00:16:01 +0600

    app file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAuth } from "shared/hooks/contextApi/useAuth";
 
 // custom imports
 import "shared/styles/globals.css";
 import "shared/styles/scrollbar.css";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  const { state } = useAuth();
+  const { user } = state;
+
+  useEffect(() => {
+    if (user?.role !== "student" && user?.role !== "teacher") {
+      navigate("/login");
+    }
+  }, [state, user, navigate]);
   return (
     <>
       <Outlet />
