7849f6699becb865d295714bafb57af5b0a99a03
programmerbanna <programmerbanna@gmail.com>
2023-03-14 22:40:20 +0600

    few modification

--- src/shared/state/userState/userReducer.js
+++ src/shared/state/userState/userReducer.js
@@ -1,22 +1,20 @@
 import { FETCH_USER_SUCCESS } from "./actionTypes";
 
-
-
 export const initialState = {
-    isLoading: false,
-    error: false,
-    user :{}
+  isLoading: false,
+  error: false,
+  user: {},
 };
 
-export const UserReducer = (state,action) => {
-    switch (action.type) {
-        case FETCH_USER_SUCCESS:
-            return {
-                ...state,
-                user:action.payload
-            }
-            
-        default: return state;
-    }
-    
-}
+export const UserReducer = (state, action) => {
+  switch (action.type) {
+    case FETCH_USER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
