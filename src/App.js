8bf48589dc66d0028365efe63c3f8506a05451ed
programmerbanna <programmerbanna@gmail.com>
2023-03-24 11:51:58 +0600

    few changes modified

--- src/App.js
+++ src/App.js
@@ -8,22 +8,11 @@ import "shared/styles/scrollbar.css";
 const App = () => {
   const navigate = useNavigate();
 
-<<<<<<< HEAD
   // useEffect(() => {
   //   if (user?.role !== "student" && user?.role !== "teacher") {
   //     navigate("/login");
   //   }
   // }, [state, user, navigate]);
-=======
-  const { state } = useAuth();
-  const { user } = state;
-
-//   useEffect(() => {
-//     if (user?.role !== "student" && user?.role !== "teacher") {
-//       navigate("/login");
-//     }
-//   }, [state, user, navigate]);
->>>>>>> 41beb16239f33c250a1450e74f024f1d73da2b74
   return (
     <>
       <Outlet />
