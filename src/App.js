e896168d89741b969827a82db3c5b04d0c61906d
programmerbanna <programmerbanna@gmail.com>
2023-03-24 11:50:06 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import { useAuth } from "shared/hooks/contextApi/useAuth";
 
 // custom imports
 import "shared/styles/globals.css";
@@ -9,14 +8,11 @@ import "shared/styles/scrollbar.css";
 const App = () => {
   const navigate = useNavigate();
 
-  const { state } = useAuth();
-  const { user } = state;
-
-  useEffect(() => {
-    if (user?.role !== "student" && user?.role !== "teacher") {
-      navigate("/login");
-    }
-  }, [state, user, navigate]);
+  // useEffect(() => {
+  //   if (user?.role !== "student" && user?.role !== "teacher") {
+  //     navigate("/login");
+  //   }
+  // }, [state, user, navigate]);
   return (
     <>
       <Outlet />
--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import UserProvider from "shared/hooks/contextApi/useAuth";
 
 // custom imports
+import store from "shared/redux/store";
 import route from "shared/route";
-
-// Create a client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: true,
-      staleTime: 60 * 1000 * 60,
-      refetchOnMount: true,
-    },
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-    <UserProvider> 
-        <RouterProvider router={route} />
-    </UserProvider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <RouterProvider router={route} />
+    </Provider>
   </React.StrictMode>
 );
