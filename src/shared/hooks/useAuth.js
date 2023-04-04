2d9c0ccd03e004635fcbbe2d384d7926f60d9876
programmerbanna <programmerbanna@gmail.com>
2023-04-04 22:16:50 +0600

    modified

--- src/shared/hooks/useAuth.js
+++ src/shared/hooks/useAuth.js
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import socket from "socket.config";
 
 export default function useAuth() {
   const auth = useSelector((state) => state?.auth);
 
-  // console.log("checking auth from auth component", auth);
+  useEffect(() => {
+    socket.emit("addUsers", auth?.user?._id);
+  }, [auth?.user?._id]);
 
   if (auth?.user) {
     return true;
