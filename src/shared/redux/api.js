5c5cf97ae31363e2b57cdfcf7efcaa2d55193aec
programmerbanna <programmerbanna@gmail.com>
2023-04-05 22:13:51 +0600

    api modified

--- src/shared/redux/api.js
+++ src/shared/redux/api.js
@@ -5,12 +5,13 @@ import { PRODUCTION_API_URL, DEVELOPMENT_API_URL, NODE_ENV } from "env";
 const API_URL =
   NODE_ENV === "production" ? PRODUCTION_API_URL : DEVELOPMENT_API_URL;
 
-console.log(".........................checking for api", NODE_ENV);
+console.log(".........................checking node env", NODE_ENV);
+console.log(".........................checking api url", API_URL);
 
 export const heiloApi = createApi({
   reducerPath: "heiloApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: API_URL + "api/v1",
+    baseUrl: API_URL,
     credentials: "include",
     prepareHeaders: async (headers, { getState, endpoint }) => {},
     postProcessResponse: async (
