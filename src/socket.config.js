0fc6adedb5742dd822861ef780b02cf8fdc14209
programmerbanna <programmerbanna@gmail.com>
2023-04-06 05:02:36 +0600

    sockt config added for testing again

--- src/socket.config.js
+++ src/socket.config.js
@@ -5,6 +5,8 @@ const API_URL =
   NODE_ENV === "production" ? PRODUCTION_SOCKET_URL : DEVELOPMENT_SOCKET_URL;
 
 console.log("APi url..............", API_URL);
+console.log("scocket url..............", PRODUCTION_SOCKET_URL);
+console.log("NOde env..............", NODE_ENV);
 
 const socket = io(API_URL);
 
