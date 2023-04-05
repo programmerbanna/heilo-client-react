243f8b77f5d65fc759d28b203d5db4db28384993
programmerbanna <programmerbanna@gmail.com>
2023-04-06 04:34:50 +0600

    newly modified

--- src/shared/redux/api.js
+++ src/shared/redux/api.js
@@ -5,19 +5,19 @@ import { PRODUCTION_API_URL, DEVELOPMENT_API_URL, NODE_ENV } from "env";
 const API_URL =
   NODE_ENV === "production" ? PRODUCTION_API_URL : DEVELOPMENT_API_URL;
 
-console.log(".........................checking node env", NODE_ENV);
-console.log(
-  ".........................checking api url",
-  API_URL,
-  PRODUCTION_API_URL
-);
-
 export const heiloApi = createApi({
   reducerPath: "heiloApi",
   baseQuery: fetchBaseQuery({
     baseUrl: API_URL,
     credentials: "include",
-    prepareHeaders: async (headers, { getState, endpoint }) => {},
+    prepareHeaders: async (headers, { getState, endpoint }) => {
+      const token = getState()?.auth?.token;
+      console.log("tokeen........", token);
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
     postProcessResponse: async (
       result,
       { dispatch, getState, requestId }
