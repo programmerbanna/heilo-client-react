10dc22a24881c661295e8242ebae800eb47aaa9c
programmerbanna <programmerbanna@gmail.com>
2023-04-05 21:53:46 +0600

    now socket is okay

--- src/socket.config.js
+++ src/socket.config.js
@@ -1,6 +1,9 @@
-import { API_URL } from "env";
+import { PRODUCTION_API_URL, DEVELOPMENT_API_URL, NODE_ENV } from "env";
 import io from "socket.io-client";
 
-const socket = io("ws://localhost:5000");
+const API_URL =
+  NODE_ENV === "production" ? PRODUCTION_API_URL : DEVELOPMENT_API_URL;
+
+const socket = io(API_URL);
 
 export default socket;
