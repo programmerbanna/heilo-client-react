8027233e9ff1ab6bb9cffdc7396ff7ef3df8b89f
programmerbanna <programmerbanna@gmail.com>
2023-04-05 22:13:45 +0600

    socket configure added

--- src/socket.config.js
+++ src/socket.config.js
@@ -1,8 +1,8 @@
-import { PRODUCTION_API_URL, DEVELOPMENT_API_URL, NODE_ENV } from "env";
+import { PRODUCTION_SOCKET_URL, DEVELOPMENT_SOCKET_URL, NODE_ENV } from "env";
 import io from "socket.io-client";
 
 const API_URL =
-  NODE_ENV === "production" ? PRODUCTION_API_URL : DEVELOPMENT_API_URL;
+  NODE_ENV === "production" ? PRODUCTION_SOCKET_URL : DEVELOPMENT_SOCKET_URL;
 
 const socket = io(API_URL);
 
