842d70584f108b7a92da0fecaf851a6ae5ff9ecb
programmerbanna <programmerbanna@gmail.com>
2023-04-05 20:24:36 +0600

    api modified

--- src/shared/redux/api.js
+++ src/shared/redux/api.js
@@ -1,8 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 import { API_URL } from "env";
-import Cookies from "js-cookie";
-import { toast } from "react-hot-toast";
 
 export const heiloApi = createApi({
   reducerPath: "heiloApi",
